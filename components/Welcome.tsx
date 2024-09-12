32962473a2a75fee5cf93d6b5b9b7b609beecb41
Luc Bertin <lbertin-ext@payplug.com>
2024-09-12 16:20:50 +0200

    new website title, favicon, footer, main h1, and 2 cols in Clients section for mobile

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,98 +1,148 @@
-'use client'
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
-import { FaGithub, FaLinkedin, FaFileAlt, FaChalkboardTeacher } from 'react-icons/fa';
-import { SiMalt } from 'react-icons/si';
-import { FaArrowRight } from 'react-icons/fa';
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaFileAlt,
+  FaChalkboardTeacher,
+} from "react-icons/fa";
+import { SiMalt } from "react-icons/si";
+import { FaArrowRight } from "react-icons/fa";
 
-const fullText = "Hello, I'm Luc Bertin. A Backend Developer.";
+const fullText = "Hello, I'm Luc. A Backend Developer :)";
 const technologies = [
-    'Python','FastAPI', 'Flask', 'Django', 'Hydra', 'Kratos', 'Keto', 'Airflow', 'Docker',
-     'Vue.js','AsyncIO', 'Websocket', 'Pydantic', 'Testing', 'CI/CD', 'AWS', 'Django','DRF',  
-     'Neo4j', 'Redis', 'SQL', 'Qt','Scraping', 'Selenium', 'R', 'DL/ML', 'Numpy/Pandas'
-  ];
-  
+  "Python",
+  "FastAPI",
+  "Flask",
+  "Django",
+  "Hydra",
+  "Kratos",
+  "Keto",
+  "Airflow",
+  "Docker",
+  "Vue.js",
+  "AsyncIO",
+  "Websocket",
+  "Pydantic",
+  "Testing",
+  "CI/CD",
+  "AWS",
+  "Django",
+  "DRF",
+  "Neo4j",
+  "Redis",
+  "SQL",
+  "Qt",
+  "Scraping",
+  "Selenium",
+  "R",
+  "DL/ML",
+  "Numpy/Pandas",
+];
+
 const clients = [
-  { name: 'Payplug', logo: '/logos/payplug2.webp' },
-  { name: 'Shadow2', logo: '/logos/shadow.jpg' },
-  { name: 'Carrier', logo: '/logos/carrier.svg' },
-  { name: 'BackMarket', logo: '/logos/backmarket.webp' },
-  { name: 'ESILV', logo: '/logos/esilv.jpg' },
-  { name: 'IIM', logo: '/logos/iim.png' },
-  { name: 'Exomind', logo: '/logos/exomind.jpg' },
-  { name: 'Flash Design', logo: '/logos/flash_design_logo.svg' }
-]
+  { name: "Payplug", logo: "/logos/payplug2.webp" },
+  { name: "Shadow2", logo: "/logos/shadow.jpg" },
+  { name: "Carrier", logo: "/logos/carrier.svg" },
+  { name: "BackMarket", logo: "/logos/backmarket.webp" },
+  { name: "ESILV", logo: "/logos/esilv.jpg" },
+  { name: "IIM", logo: "/logos/iim.png" },
+  { name: "Exomind", logo: "/logos/exomind.jpg" },
+  { name: "Flash Design", logo: "/logos/flash_design_logo.svg" },
+];
 
 export default function StylishIntroductionPage() {
-    const [displayText, setDisplayText] = useState('');
-  
-    useEffect(() => {
-      let i = 0;
-      const typingEffect = setInterval(() => {
-        if (i <= fullText.length) {
-          setDisplayText(fullText.slice(0, i));
-          i++;
-        } else {
-          clearInterval(typingEffect);
-        }
-      }, 100);
-  
-      return () => clearInterval(typingEffect);
-    }, []);
-  
-    return (
-      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-blue-900 text-white">
-        <main className="container mx-auto px-4 py-16 relative">
-          {/* Hero Section */}
-          <section className="mb-24">
-            <div className="flex flex-col md:flex-row items-center justify-between">
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-                className="md:w-1/2"
-              >
-                <h1 className="text-5xl font-bold mb-6 h-48 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500 leading-tight">
-                  {displayText}
-                </h1>
-                <p className="text-3xl mb-12 text-gray-300">
-                  Passionate about building robust and scalable backend systems that power the digital world.
-                </p>
-  
-                {/* Buttons Section */}
-                <div className="flex flex-wrap justify-start gap-6">
-                  {[
-                    { href: "https://github.com/Luc-Bertin", icon: FaGithub, label: "GitHub" },
-                    { href: "https://www.malt.fr/profile/lucbertin1", icon: SiMalt, label: "Malt" },
-                    { href: "https://www.linkedin.com/in/luc-bertin-291904ab/", icon: FaLinkedin, label: "LinkedIn" },
-                    { href: "/resume_Luc.pdf", icon: FaFileAlt, label: "Resume/CV" },
-                    { href: "https://blog.lucbertin.com/", icon: FaChalkboardTeacher, label: "Classes I Teach" }
-                  ].map((link, index) => (
-                    <motion.div
-                      key={link.label}
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      transition={{ duration: 0.5, delay: index * 0.1 }}
-                      className="group"
+  const [displayText, setDisplayText] = useState("");
+
+  useEffect(() => {
+    let i = 0;
+    const typingEffect = setInterval(() => {
+      if (i <= fullText.length) {
+        setDisplayText(fullText.slice(0, i));
+        i++;
+      } else {
+        clearInterval(typingEffect);
+      }
+    }, 100);
+
+    return () => clearInterval(typingEffect);
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-black to-indigo-900 text-white">
+      <main className="container mx-auto px-4 py-16 relative">
+        {/* Hero Section */}
+        <section className="mb-24">
+          <div className="flex flex-col md:flex-row items-center justify-between">
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              className="md:w-1/2"
+            >
+              <h1 className="text-5xl font-bold mb-6 h-48 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500 leading-tight">
+                {displayText}
+              </h1>
+              <p className="text-3xl mb-12 text-gray-300">
+                Passionate about building robust and scalable backend systems
+                that power the digital world.
+              </p>
+
+              {/* Buttons Section */}
+              <div className="flex flex-wrap justify-start gap-6">
+                {[
+                  {
+                    href: "https://github.com/Luc-Bertin",
+                    icon: FaGithub,
+                    label: "GitHub",
+                  },
+                  {
+                    href: "https://www.malt.fr/profile/lucbertin1",
+                    icon: SiMalt,
+                    label: "Malt",
+                  },
+                  {
+                    href: "https://www.linkedin.com/in/luc-bertin-291904ab/",
+                    icon: FaLinkedin,
+                    label: "LinkedIn",
+                  },
+                  {
+                    href: "/resume_Luc.pdf",
+                    icon: FaFileAlt,
+                    label: "Resume/CV",
+                  },
+                  {
+                    href: "https://blog.lucbertin.com/",
+                    icon: FaChalkboardTeacher,
+                    label: "Classes I Teach",
+                  },
+                ].map((link, index) => (
+                  <motion.div
+                    key={link.label}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: index * 0.1 }}
+                    className="group"
+                  >
+                    <Link
+                      href={link.href}
+                      className="flex items-center gap-2 text-lg px-6 py-3 rounded-full bg-gradient-to-r from-indigo-800 to-blue-900 text-white font-semibold shadow-lg transition-all duration-300 transform group-hover:scale-105"
                     >
-                      <Link
-                        href={link.href}
-                        className="flex items-center gap-2 text-lg px-6 py-3 rounded-full bg-gradient-to-r from-indigo-800 to-blue-900 text-white font-semibold shadow-lg transition-all duration-300 transform group-hover:scale-105"
-                      >
-                        <link.icon className="text-white" />
-                        <span className="mr-2">{link.label}</span>
-                        <span className="inline-block w-0 group-hover:w-5 opacity-0 group-hover:opacity-100 transition-all duration-300">
-                          <FaArrowRight className="ml-1" />
-                        </span>
-                      </Link>
-                    </motion.div>
-                  ))}
-                </div>
-              </motion.div>
-  
-              <motion.div
+                      <link.icon className="text-white" />
+                      <span className="mr-2">{link.label}</span>
+                      <span className="inline-block w-0 group-hover:w-5 opacity-0 group-hover:opacity-100 transition-all duration-300">
+                        <FaArrowRight className="ml-1" />
+                      </span>
+                    </Link>
+                  </motion.div>
+                ))}
+              </div>
+            </motion.div>
+
+            <motion.div
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.5 }}
@@ -109,9 +159,9 @@ export default function StylishIntroductionPage() {
                 />
               </div>
             </motion.div>
-            </div>
-          </section>
-  
+          </div>
+        </section>
+
         {/* Tech Stack Section */}
         <motion.section
           className="mb-24"
@@ -128,7 +178,7 @@ export default function StylishIntroductionPage() {
               <div
                 key={index}
                 className="flex items-center text-[2rem] font-black text-gray-200"
-                style={{ fontFamily: 'Arial Black, Impact, sans-serif' }}
+                style={{ fontFamily: "Arial Black, Impact, sans-serif" }}
               >
                 <span className="flex items-baseline">
                   {tech}
@@ -148,9 +198,9 @@ export default function StylishIntroductionPage() {
           transition={{ duration: 0.6 }}
         >
           <h2 className="text-5xl font-bold mb-8 text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500">
-            Clients I have Worked With
+            Clients I Have Worked With
           </h2>
-          <div className="flex flex-wrap justify-center gap-8">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
             {clients.map((client, index) => (
               <motion.div
                 key={client.name}
@@ -173,7 +223,7 @@ export default function StylishIntroductionPage() {
 
         {/* Booking Section */}
         <motion.section
-          className="text-center"
+          className="mb-24 text-center"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
@@ -182,7 +232,9 @@ export default function StylishIntroductionPage() {
           <h2 className="text-5xl font-bold mb-8 text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500">
             Ready to Collaborate?
           </h2>
-          <p className="text-xl mb-8 text-gray-300">Turn your ideas into reality: schedule a meeting with me.</p>
+          <p className="text-xl mb-8 text-gray-300">
+            Turn your ideas into reality: schedule a meeting with me.
+          </p>
           <Link
             href="https://calendly.com/lucbertin/meeting"
             className="inline-block bg-blue-600 text-white px-8 py-4 rounded-full text-lg font-semibold hover:bg-blue-700 transition-colors shadow-lg hover:shadow-xl transform hover:-translate-y-1"
@@ -191,8 +243,11 @@ export default function StylishIntroductionPage() {
           </Link>
         </motion.section>
       </main>
-
-      
+      <footer className="py-20 w-full bg-black">
+        <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
+          Luc Bertin © 2024
+        </p>
+      </footer>
     </div>
   );
 }
